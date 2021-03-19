4db6a2ad3cd1e885317301bacdb39833c712de30
Maicon Rodrigues <maiconrodriguessc@gmail.com>
2021-03-19 14:58:55 -0300

    Add new items

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,7 +1,19 @@
 const cheatMeals = [{
-    name: "Hamburger",
-    img: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
-}
-        ];
+        name: "Hamburger",
+        img: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"    
+    },
+    {
+        name: "Pizza",
+        img: "https://images.unsplash.com/photo-1601924582970-9238bcb495d9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80"
+    },
+    {
+        name: "Pancakes",
+        img: "https://images.unsplash.com/photo-1509482560494-4126f8225994?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=500&q=80"
+    },
+    {
+        name: "Icecream",
+        img: "https://images.unsplash.com/photo-1546470427-0d4db154ceb7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
+    }
+];
 
 export {cheatMeals}; 
