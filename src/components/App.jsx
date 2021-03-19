6062bd2dca9ad21ab71a36a074b612c93fab89ba
Maicon Rodrigues <maiconrodriguessc@gmail.com>
2021-03-19 10:50:37 -0300

    Fix error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,22 +9,8 @@ function App() {
 
     return(
       <div>
-<<<<<<< HEAD
-        <Header/> 
-=======
-        {/* <Header/> */}
-        {/* <Show/> */}
-        {ready ? (
-          <GetMeal/>
-        ) : (
-          <div>
-            <h1 className="main-heading">Let me make this important decision for you...</h1>
-            <button className="btn-main" onClick={start}>{text}</button>
-          </div>
-        )}
-        
+        <Header/>
         {/* <Footer/> */}
->>>>>>> 061107ace90dcfaec1617e0b93be352e125609c7
       </div>
     )
     
